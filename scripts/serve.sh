dce040e8d0673b42528c097af075389daf7b0f6a
Francisco Mendoza <fco.mendoza@boosteriit.com>
2023-11-14 16:52:45 -0500

    Add changes

---
+++ scripts/serve.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Descargar los últimos cambios de la rama "develop"
+git pull origin develop
+
+# Generar código de producción
+npx nx run webapp:build
+
+# pm2 stop main
+# pm2 delete main
+
+# pm2 start ./dist/apps/webapp/npm --name "main" -- start
