950b7050e4e0e5ebf7fa2cbbe7875d99ccc6981c
Francisco Mendoza <fco.mendoza@boosteriit.com>
2023-11-16 12:03:45 -0500

    Add changes

--- scripts/serve.sh
+++ scripts/serve.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Obtén la ruta del directorio actual de trabajo
+current_dir=$(pwd)
+
+# Verifica si el directorio actual es el directorio raíz (contiene nx.json)
+if [ -e "$current_dir/nx.json" ]; then
+    echo "Este es el directorio raíz."
+else
+    echo "Este no es el directorio raíz. El script se detendrá."
+    exit 1
+fi
+
 # Descargar los últimos cambios de la rama "develop"
 git pull origin develop
 
@@ -12,9 +23,7 @@ npx nx run webapp:build
 pm2 stop main
 pm2 delete main
 
-root_dir=$(pwd)
-
 cd ./dist/apps/webapp
 pm2 start npm --name "main" -- start
 
-cd $root_dir
+cd $current_dir
